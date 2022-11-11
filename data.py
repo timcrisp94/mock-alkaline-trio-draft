7c77e79d6a44f7fed30db54fdd860fb208da5e0f
Tim Crisp <timothycrisp@gmail.com>
2022-11-10 22:12:18 -0600

    build one round draft simulation with random song functions, test player variables, play round function successfully prints results for one round of game play

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ from csv import DictReader
 
 
 with open("draft_data.csv", 'r') as f:
-  dict_reader = DictReader(f)
-  songs_list = list(dict_reader)
+    dict_reader = DictReader(f)
+    songs_list = list(dict_reader)
 
 
 
