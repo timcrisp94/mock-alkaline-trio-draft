572ad98a5e0f734664e712d66fb8783fd85073c2
Tim Crisp <timothycrisp@gmail.com>
2022-11-10 19:30:42 -0600

    touch draft_data.csv and csv.py, use csv module to print draft data

---
+++ csv.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('draft_data.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
