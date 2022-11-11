f3869b64af5f9a314176d74069edfd5875131a8f
Tim Crisp <timothycrisp@gmail.com>
2022-11-11 12:50:55 -0600

    touch draft_simulation dot py, migrate code from main

--- main.py
+++ main.py
@@ -2,23 +2,9 @@ from data import songs_list
 from time import sleep
 import random
 
-for song in songs_list:
-    category = song['category']
-    title = song['song']
-
-def pick_random_song(player_name):
-    random_song = random.choice(songs_list)
-    category = random_song['category']
-    song_title = random_song['song']
-    print(f"In the {category} category, {player_name} picks {song_title}")
-
-players = ["Tim", "David", "Jeremy", "Jim"]
-
-def play_round():
-    for p in players:
-        sleep(1)
-        pick_random_song(p)
-
-
-print("Welcome to the draft, sluts")
-play_round()
+# print(songs_list)
+# prints [{'category': 'Asian_Man', 'song': '"Radio"'}, {et al}]
+# print(songs_list[0])
+# prints {'category': 'Asian_Man', 'song': '"Radio"'}
+print(songs_list[0]['category'])
+# prints Asian_Man
